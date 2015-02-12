ae7a93b0eab93edbb2d17e789d9d56e3c11e93b6
Scott Luinski <cuda318cid@yahoo.com>
2015-02-12 10:20:23 -0500

    .

---
+++ Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseArray
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] origArray = new int[10];
+            CreateArray(origArray);
+            Printer(ReverseArray(origArray));
+
+            Console.ReadKey();
+
+        }
+
+
+        static int[] CreateArray(int[] a)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                a[i] = i + 1;
+            }
+                return a;
+        }
+
+        static int[] ReverseArray(int[] a)
+        {
+            int[] b = new int[10];
+            int bIndex = 9;
+
+            for (int i = 0; i < 10; i++ )
+            {
+                b[i] = a[bIndex--];
+            }
+
+                return b;
+        }
+
+
+        static void Printer(int[] a)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("{0} ", a[i]);
+            }
+        }
+
+
+
+
+
+
+
+
+
+    }
+}
---
+++ README.txt
@@ -0,0 +1,3 @@
+Homework - Use methods to create an array, reverse it, and "print" it.
+Not impressive but I am starting to just put everything on here for practice.
+Also, sometimes I want to reference stuff from work and this makes that possible.
